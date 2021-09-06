3dd8c8b803446ecddf31603dfbe1fce35d6042ba
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2021-09-06 20:24:06 +0300

    Add project files.

---
+++ AracHasarEngelleme/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+namespace AracHasarEngelleme
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+            hasarAcik = true;
+            tekerlekHasarAcik = true;
+        }
+
+        public bool hasarAcik;
+        public bool tekerlekHasarAcik;
+    }
+}
---
+++ AracHasarEngelleme/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using Steamworks;
+
+namespace AracHasarEngelleme
+{
+    public class Main : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            VehicleManager.onDamageVehicleRequested += new DamageVehicleRequestHandler(aracHasar);
+            VehicleManager.onDamageTireRequested += new DamageTireRequestHandler(tekerlekHasar);
+        }
+
+        protected override void Unload()
+        {
+            
+        }
+
+        private void aracHasar(CSteamID instigatorSteamID, InteractableVehicle vehicle, ref ushort pendingTotalDamage, ref bool canRepair, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+
+            if (Configuration.Instance.hasarAcik)
+            {
+                shouldAllow = false;
+            }
+            else
+            {
+                shouldAllow = true;
+            }
+        }
+
+        private void tekerlekHasar(CSteamID instigatorSteamID, InteractableVehicle vehicle, int tireIndex, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+            if (Configuration.Instance.tekerlekHasarAcik)
+            {
+                shouldAllow = false;
+            }
+            else
+            {
+                shouldAllow = true;
+            }
+        }
+
+    }
+}
